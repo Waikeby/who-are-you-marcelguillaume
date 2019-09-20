2c34972d38827660c0b8a55ccfd8e28dd8d2963d
Guillaume Marcel <guillaumemarcel@wifi-eduroam-etud-238-37.unilim.fr>
2019-09-20 08:56:46 +0200

    add push final

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,2 +1,38 @@
-// Your script goes here
-console.log("Hello World");
+
+$(document).scroll(function () {
+            var sc = $(document).scrollTop();
+
+            if(sc >= 585 && sc <=1297){
+              $(".vieperso").addClass("active");
+              $(".viepro").removeClass("active");
+              $(".galerie").removeClass("active");
+              $(".contact").removeClass("active");
+            }
+    
+             if(sc >= 1298 && sc <=1700){
+              $(".vieperso").removeClass("active");
+              $(".viepro").addClass("active");
+              $(".galerie").removeClass("active");
+              $(".contact").removeClass("active");
+            }
+    
+            if(sc >= 1701 && sc <=1942){
+              $(".vieperso").removeClass("active");
+              $(".viepro").removeClass("active");
+              $(".galerie").addClass("active");
+              $(".contact").removeClass("active");
+            }
+    
+         if(sc > 1943){
+              $(".vieperso").removeClass("active");
+              $(".viepro").removeClass("active");
+              $(".galerie").removeClass("active");
+              $(".contact").addClass("active");
+            }
+    
+        if(sc < 584){
+              $(".vieperso").removeClass("active");
+              $(".viepro").removeClass("active");
+              $(".galerie").removeClass("active");
+              $(".contact").removeClass("active");
+            }})
