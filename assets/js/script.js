4dee9d99d538587860b773ec0e50060468430b8f
Guillaume Marcel <marcelguillaume54@gmail.com>
2019-10-03 15:38:33 +0200

    add push final aprés review

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,38 +1,37 @@
+    $(document).scroll(function () {
+        var sc = $(document).scrollTop();
 
-$(document).scroll(function () {
-            var sc = $(document).scrollTop();
+        if (sc >= 798 && sc <= 1212) {
+            $(".vieperso").addClass("active");
+            $(".viepro").removeClass("active");
+            $(".galerie").removeClass("active");
+            $(".acceuil").removeClass("active");
+        }
 
-            if(sc >= 585 && sc <=1297){
-              $(".vieperso").addClass("active");
-              $(".viepro").removeClass("active");
-              $(".galerie").removeClass("active");
-              $(".contact").removeClass("active");
-            }
-    
-             if(sc >= 1298 && sc <=1700){
-              $(".vieperso").removeClass("active");
-              $(".viepro").addClass("active");
-              $(".galerie").removeClass("active");
-              $(".contact").removeClass("active");
-            }
-    
-            if(sc >= 1701 && sc <=1942){
-              $(".vieperso").removeClass("active");
-              $(".viepro").removeClass("active");
-              $(".galerie").addClass("active");
-              $(".contact").removeClass("active");
-            }
-    
-         if(sc > 1943){
-              $(".vieperso").removeClass("active");
-              $(".viepro").removeClass("active");
-              $(".galerie").removeClass("active");
-              $(".contact").addClass("active");
-            }
-    
-        if(sc < 584){
-              $(".vieperso").removeClass("active");
-              $(".viepro").removeClass("active");
-              $(".galerie").removeClass("active");
-              $(".contact").removeClass("active");
-            }})
+        if (sc >= 1213 && sc <= 1629) {
+            $(".vieperso").removeClass("active");
+            $(".viepro").addClass("active");
+            $(".galerie").removeClass("active");
+            $(".acceuil").removeClass("active");
+        }
+
+        if (sc >= 1630 && sc <= 1942) {
+            $(".vieperso").removeClass("active");
+            $(".viepro").removeClass("active");
+            $(".galerie").addClass("active");
+            $(".acceuil").removeClass("active");
+        }
+
+
+
+        if (sc < 584) {
+            $(".vieperso").removeClass("active");
+            $(".viepro").removeClass("active");
+            $(".galerie").removeClass("active");
+            $(".acceuil").addClass("active");
+        }
+    })
+
+    $("#hamburger").click(function () {
+        $('nav ul').fadeToggle();
+    })
